cc11eaf5cfbb713ed38e51256a38c5aac3247fc7
Fabrice BEYA <byxfab001@myuct.ac.za>
2019-01-25 14:15:55 +0200

    Update index.js
    Added Azure db connection

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ var express             = require("express"),
     port                = process.env.PORT || 3001;
 
 
-mongoose.connect('mongodb://localhost/mkiot', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost/mkiot', { useNewUrlParser: true });
+
+mongoose.connect("mongodb://tokenbar.documents.azure.com:10255/?ssl=true&replicaSet=globaldb" || database.localUrl,{
+    auth:{  
+        user:"tokenbar",
+        password:"IcLSCWlDrDdIA4AXz4aamdOcixvL8rOLbHzvGWnRq4sRsUMj1ad33REFpRyFAt7TncWzMWmPlyhMs5nxo5w62A==",
+    }}, function(err, db){
+        
+    });
 
 app.use(bodyParser.json());
     
