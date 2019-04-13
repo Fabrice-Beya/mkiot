90878e1939d976874c5a6174634dfa449244335e
Fabrice BEYA <beyafabrice@gmail.com>
2019-04-13 09:43:32 +0200

    Change mongodb server to mlabs

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ var express             = require("express"),
     mongoose            = require('mongoose'),
     bodyParser          = require("body-parser"),
     Obd                 = require("./models/odb"),
-    port                = process.env.PORT || 8080;
+    port                = process.env.PORT || 3000;
 
 
 //mongoose.connect('mongodb://localhost/mkiot', { useNewUrlParser: true });
 
-mongoose.connect("mongodb://mkiot.documents.azure.com:10255/?ssl=true" || database.localUrl,{
-    auth:{  
-        user:"mkiot",
-        password:"mzeMQp2hRHR31bV6J8G6E9eDV63UKwW6jFz2NJYYJwXAXhkGcnc8qdANA6rrXjRM7yb3YShhVMqbaL3B3NLCIQ==",
-    }}, function(err, db){
+// mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds243812.mlab.com:43812/tokenbar" || database.localUrl,{
+//     auth:{  
+//         user:"mkiot",
+//         password:"mzeMQp2hRHR31bV6J8G6E9eDV63UKwW6jFz2NJYYJwXAXhkGcnc8qdANA6rrXjRM7yb3YShhVMqbaL3B3NLCIQ==",
+//     }}, function(err, db){
         
-    });
+//     });
+// Set up mongoose connection
+
+let dev_db_url = 'mongodb://sensurobd:sensur001@ds243812.mlab.com:43812/tokenbar';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 app.use(bodyParser.json());
