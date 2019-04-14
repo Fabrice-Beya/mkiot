ed5ae402c9542c286861f7124eb910ce21b9f17b
Fabrice BEYA <beyafabrice@gmail.com>
2019-04-14 13:09:10 +0200

    Update collection model
    removed data

--- models/odb.js
+++ models/odb.js
@@ -1,7 +1,18 @@
 var mongoose    = require("mongoose");
 
 var obdSchema = mongoose.Schema({
-   data : {type: String, default:''},
+   vin   :{type: String, default:''},
+   sim_id   :{type: String, default:''},
+   device_id   :{type: String, default:''},
+   speed   :{type: String, default:''},
+   rpm   :{type: String, default:''},
+   engine_load   :{type: String, default:''},
+   throttle   :{type: String, default:''},
+   intake_temp   :{type: String, default:''},
+   coolant_temp   :{type: String, default:''},
+   barometric   :{type: String, default:''},
+   ambient_temp   :{type: String, default:''},
+   gps      :{type: String, default:''}
 });
 
 module.exports = mongoose.model("Obd", obdSchema);
