9132ecfb7fba54ffc6b4726744c2a66036417ee9
Fabrice BEYA <beyafabrice@gmail.com>
2019-04-16 16:26:11 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -5,25 +5,16 @@ var express             = require("express"),
     Obd                 = require("./models/odb"),
     port                = process.env.PORT || 3000;
 
-
+// local mongodb server
 //mongoose.connect('mongodb://localhost/mkiot', { useNewUrlParser: true });
 
-// mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds243812.mlab.com:43812/tokenbar" || database.localUrl,{
-//     auth:{  
-//         user:"mkiot",
-//         password:"mzeMQp2hRHR31bV6J8G6E9eDV63UKwW6jFz2NJYYJwXAXhkGcnc8qdANA6rrXjRM7yb3YShhVMqbaL3B3NLCIQ==",
-//     }}, function(err, db){
-        
-//     });
 // Set up mongoose connection
-
 let dev_db_url = 'mongodb://sensurobd:sensur001@ds243812.mlab.com:43812/tokenbar';
 let mongoDB = process.env.MONGODB_URI || database.localUrl;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
 
 app.use(bodyParser.json());
     
